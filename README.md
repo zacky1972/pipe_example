a932187f19acd0faba33b47d6f5acdccf17395e6
Susumu Yamazaki <zacky1972@gmail.com>
2018-06-02 02:44:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# pipe_example
+
+Pipe example for Rust: a process forks and connects a child process with pipes.
+ 
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate libc;
+
+use libc::{c_void, size_t, pipe, fork, close, write, read};
+
+
+fn main() {
+	let mut pp = [0; 2];
+	let mut qq = [0; 2];
+
+	unsafe {
+		pipe(pp.as_mut_ptr());
+		pipe(qq.as_mut_ptr());
+
+		match fork() {
+			0 => {
+				// 子プロセス
+				close(pp[1]);
+				close(qq[0]);
+
+				let mut data: [u8; 256] = [0; 256];
+				let mut buf = data.as_mut_ptr() as *mut c_void;
+				read(pp[0], buf, 256);
+				for i in data.iter_mut() {
+					*i = *i + 1;
+				}
+				write(qq[1], buf, (data.len()) as size_t);
+			},
+			_ => {
+				// 親プロセス
+				close(pp[0]);
+				close(qq[1]);
+
+				let src_data: [u8; 10] = [1; 10];
+				let src_buf = src_data.as_ptr() as *const c_void;
+				write(pp[1], src_buf, (src_data.len()) as size_t);
+
+				let mut dst_data: [u8; 256] = [0; 256];
+				let mut dst_buf = dst_data.as_mut_ptr() as *mut c_void;
+				read(qq[0], dst_buf, 256);
+				for x in dst_data.iter() {
+					print!("{} ", x);
+				}
+			},
+		}
+	}
+}
